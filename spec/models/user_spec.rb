ebde00bd1c26889143f626157a6e6c71dfccb6b0
takumitina <takumi_c-12a@stu.kbc.ac.jp>
2014-08-12 12:22:23 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -107,5 +107,20 @@ describe User do
   end
     end
   end
+
+    it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 end
 
